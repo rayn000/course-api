26623115f7185d0113a1930e48dd8629394facb2
suraj <surajrao.2325@gmail.com>
2024-05-06 19:43:40 +0530

    Initial commit

---
+++ src/main/java/io/springboot/course/CourseService.java
@@ -0,0 +1,59 @@
+package io.springboot.course;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourseService {
+	
+	@Autowired
+	private CourseRepository courseRepository;
+	
+	public List<Course> getAllCourses(String topicId) {
+		List<Course> courses = new ArrayList();
+		courseRepository.findByTopicId(topicId)
+		.forEach(courses::add);
+		
+		return courses;
+	}
+	
+	public Course getCourse(String id) {
+		Optional<Course> optionalCourse = courseRepository.findById(id);
+		if (optionalCourse.isPresent()) {
+			return optionalCourse.get();
+		}
+
+		return null;
+	}
+	
+	public void addCourse(Course course) {
+		courseRepository.save(course);
+	}
+
+	public void updateCourse(Course course) {		
+		courseRepository.save(course);
+	}
+
+	public void deleteCourse(String id) {
+		courseRepository.deleteById(id);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
